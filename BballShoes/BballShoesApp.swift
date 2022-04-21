5ec3d76d19e40f6328861e269857154adcf97ef9
k1jeong <kjeong522@gmail.com>
2022-04-22 01:33:25 +0900

    Initial Commit

---
+++ BballShoes/BballShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BballShoesApp.swift
+//  BballShoes
+//
+//  Created by Kwon Young Jeong on 2022/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BballShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BballShoes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BballShoes
+//
+//  Created by Kwon Young Jeong on 2022/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
